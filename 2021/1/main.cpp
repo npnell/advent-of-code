4727166f498d03b7aa38a27fc0ec90b9b47aa7ad
npnell <rsaha221100@gmail.com>
2022-05-16 03:43:51 +0530

    finished 2021 day1-part1

--- 2021/1/main.cpp
+++ 2021/1/main.cpp
@@ -1,7 +1,42 @@
 // Advent of Code 2021
 // Day 1: Sonar Sweep
 
+#include <typeinfo>
+#include "read.h"
+
+template<typename T>
+void log(const T &output)
+{
+    std::cout << output << std::endl;
+}
+
+unsigned long sweep(const std::vector<int> &depths)
+{
+    unsigned long depth_increments = 0;
+    if(begin(depths) != end(depths)) {
+        for(auto iter = begin(depths); iter != end(depths) - 1; ++iter) {
+            if(*iter < *(iter + 1)) {
+                depth_increments = depth_increments + 1;
+            }
+        }   
+    }
+    return depth_increments;
+}
+
+int initialize(void) {
+    std::vector<char> buffer;
+    if(read_file(buffer, "input.txt")) {
+        exit(EXIT_FAILURE);
+    }
+    auto depths = tokenize_buffer(buffer);
+    auto depth_increments = sweep(depths);
+    
+    log<decltype(depth_increments)>(depth_increments);
+
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char *argv[])
 {
-    
+    initialize();
 }
---
+++ 2021/1/read.h
@@ -0,0 +1,57 @@
+#ifndef READ_H
+#define READ_H
+
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+int read_file(std::vector<char> &buffer, std::string filename)
+{
+    std::ifstream instream(filename, std::ios::in | std::ios::binary | std::ios::ate);
+    if(instream) {
+        std::size_t size = instream.tellg();
+        instream.seekg(0, std::ios::beg);
+
+        buffer.resize(size);
+        instream.read(buffer.data(), size);
+        if(!instream) {
+            std::cerr << "failed to read file: " + filename + "\n";
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    } else {
+        std::cerr << "failed to open file: " + filename + "\n";
+        return EXIT_FAILURE;
+    }
+}
+
+int extract_token(const std::vector<char>::const_iterator &lexeme_begin, const std::vector<char>::const_iterator &forward)
+{
+    std::string token(lexeme_begin, forward);
+    return std::stoi (token);
+}
+
+std::vector<int> tokenize_buffer(const std::vector<char> &buffer)
+{
+    std::vector<char>::const_iterator lexeme_begin = begin(buffer);
+    std::vector<char>::const_iterator forward = begin(buffer);
+    std::vector<int> tokens;
+
+    if(begin(buffer) != end(buffer)) {
+        while(forward != end(buffer)) {
+            if(*forward == '\n' || *forward == '\0' || *forward == ' ' || *forward == EOF) {
+                tokens.push_back(extract_token(lexeme_begin, forward));
+                lexeme_begin = forward + 1;
+            }
+            ++forward;
+        }
+        if(forward != lexeme_begin)
+            tokens.push_back(extract_token(lexeme_begin, forward));
+    }
+    return tokens;
+}
+
+#endif
